f56450c2ea14da11ab21fb9d81ee41df9a276d6d
Chris Stacpoole <chris.stacpoole@gmail.com>
2019-01-07 21:47:50 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
-// const { Client, Attachment } = require('discord.js')
-// const client = new Client()
+const { Client, Attachment } = require('discord.js')
+const client = new Client()
 
-// require('dotenv').config();
+require('dotenv').config();
 
-// // Import the native fs module
-// const fs = require('fs');
+// Import the native fs module
+const fs = require('fs');
 
 
-// client.on('ready', () => {
-//   console.log(`Logged in as ${client.user.tag}!`);
-// });
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-// client.on('message', msg => {
-//   if (msg.content === '!phil') {
+client.on('message', msg => {
+  if (msg.content === '!phil') {
 
-//     var files = fs.readdirSync('./images');
-//     let chosenFile = files[Math.floor(Math.random() * files.length)]; 
-//     console.log(chosenFile);
+    var files = fs.readdirSync('./images');
+    let chosenFile = files[Math.floor(Math.random() * files.length)]; 
+    console.log(chosenFile);
 
-//     const buffer = fs.readFileSync('./images/' + chosenFile);
-//     msg.channel.send(new Attachment(buffer, chosenFile));
-//   }
-// });
+    const buffer = fs.readFileSync('./images/' + chosenFile);
+    msg.channel.send(new Attachment(buffer, chosenFile));
+  }
+});
 
-// client.login(process.env.DISCORD_TOKEN);
+client.login(process.env.DISCORD_TOKEN);
 
